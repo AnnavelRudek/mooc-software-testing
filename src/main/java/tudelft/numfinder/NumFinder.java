112fe8e1c7a2873bb6f864f0f15e7e72a8397251
AnnavelRudek <anna.tuszynska1@gmail.com>
2018-11-16 13:52:54 +0100

    update

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -4,12 +4,15 @@ public class NumFinder {
     private int smallest = Integer.MAX_VALUE;
     private int largest = Integer.MIN_VALUE;
 
+
     public void find(int[] nums) {
         for(int n : nums) {
 
             if(n < smallest)
                 smallest = n;
-            else if (n > largest)
+
+            // "else if" doesn't work correctly
+            if (n > largest)
                 largest = n;
 
         }
@@ -22,4 +25,5 @@ public class NumFinder {
     public int getLargest () {
         return largest;
     }
+
 }
